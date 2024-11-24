2cdebe3b1a946955ec7564a21d3ed4988c0e46bb
Roxigers <james.him@efrei.net>
2024-11-24 21:06:57 +0100

    création times.c et times.h qui permette de calculer la comlexité

---
+++ times.c
@@ -0,0 +1,23 @@
+//
+// Created by HIM on 24/11/2024.
+//
+#include "times.h"
+
+clock_t start_time()
+{
+    clock_t start = clock();
+    return start;
+}
+
+clock_t end_time()
+{
+    clock_t end = clock();
+    return end;
+}
+
+void print_time(clock_t start, clock_t end)
+{
+    double time_taken = ((double)end - start) / CLOCKS_PER_SEC;
+    printf("Time taken: %f seconds\n", time_taken);
+    printf("Time taken: %f milliseconds\n", time_taken * 1000);
+}
---
+++ times.h
@@ -0,0 +1,16 @@
+//
+// Created by HIM on 24/11/2024.
+//
+
+#ifndef PROJECT_ROVER_TIMES_H
+#define PROJECT_ROVER_TIMES_H
+#include <time.h>
+#include <stdio.h>
+
+clock_t start_time();
+
+clock_t end_time();
+
+void print_time(clock_t start, clock_t end);
+
+#endif //PROJECT_ROVER_TIMES_H
